57896b73247eec8cf323178944479bfc35cdc59b
hemxx21 <126137540+hemxx21@users.noreply.github.com>
2025-01-06 21:24:02 +0530

    Create Secondmaxdigit.java

---
+++ Secondmaxdigit.java
@@ -0,0 +1,31 @@
+public class Secondmaxdigit{
+  public static void main(String[] args){
+    int a=10;
+    int b=23;
+    int c=4;
+    if (a>b && a>c){
+      if (b>c){
+        System.out.println("the second largest number is"+b)
+      }
+      else{
+        System.out.println("the second largest number is"+c)
+      }
+    }
+    else if (b>a && b>c){
+      if (a>c){
+        System.out.println("the second largest number is"+a)
+      }
+      else{
+        System.out.println("the second largest number is"+c)
+      }
+    }
+    else if (c>a && c>b) {
+      if (a>b){
+        System.out.println("the second largest number is"+a)
+      }
+      else{
+        System.out.println("the second largest number is"+b)
+      }
+    }
+  }
+}
